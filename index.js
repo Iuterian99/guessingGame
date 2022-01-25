a77df90ca9547649e21e381be0ad32de64986bd9
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-25 18:24:07 +0500

    finished

--- index.js
+++ index.js
@@ -13,19 +13,52 @@ is a special object that is the entry point to the DOM!
 ! "WEB APIs" 
 are basically libraries that are written in Javascript that are automatically available for us to use
 */
-const input = Number(document.querySelector('.guess').value);
-const message = document.querySelector('.message');
-const check = document.querySelector('.check');
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
 
-check.addEventListener('click', function () {
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+let number = 20;
+let highScore = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  const input = Number(document.querySelector('.guess').value);
   if (!input) {
-    message.textContent = '⛔ No Number!';
+    document.querySelector('.message').textContent = '⛔ No Number!';
   } else if (randomNumber === input) {
-    message.textContent = '🎉 You Won';
-  } else if (input <= randomNumber) {
-    message.textContent = 'too low';
-  } else {
-    message.textContent = 'too big';
+    document.querySelector('.message').textContent = '🎉 You Won';
+    document.querySelector('.number').textContent = randomNumber;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '200px';
+  } else if (input < randomNumber) {
+    if (number > 1) {
+      document.querySelector('.message').textContent = '📉 too low';
+      number--;
+      document.querySelector('.score').textContent = number;
+    } else {
+      document.querySelector('.message').textContent = '🎇 You Lost';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'black';
+    }
+  } else if (input > randomNumber) {
+    if (number > 1) {
+      document.querySelector('.message').textContent = '📈 too high';
+      number--;
+      document.querySelector('.score').textContent = number;
+    } else {
+      document.querySelector('.message').textContent = '🎇 You Lost';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'black';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#031461';
+  document.querySelector('.guess').value = null;
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start Guessing . . .';
+  if (number === 20) {
+    document.querySelector('.highscore').textContent = 0;
+  } else if (number > highScore) {
+    document.querySelector('.highscore').textContent = number;
   }
 });
